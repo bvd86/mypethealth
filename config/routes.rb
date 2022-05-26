163151d0f537eab28c0789d9cb889fcc7c3f2ed4
Jonathan Jenin <jonathan.jenin@gmail.com>
2022-05-26 19:21:48 -0400

    Added comment on routes.

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
     # Routes for pets
     resources :pets, except: :show
   end
+
+  # Users custom routes
   patch '/make_available/', to: 'users#make_available'
   patch '/make_unavailable/', to: 'users#make_unavailable'
-
   get '/my-profile/', to: 'users#my_profile', as: 'my-profile'
 
   # Routes for consultations
@@ -18,6 +19,8 @@ Rails.application.routes.draw do
     resources :messages, only: :create
   end
 
+  # Consultations custom routes
   get '/my-consultations/', to: 'consultations#my_consultations', as: 'my-consultations'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
