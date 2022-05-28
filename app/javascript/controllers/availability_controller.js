03992137f5d993f24985e8d89af1011af0c34172
Wendy Pham <wendypham_s2@hotmail.com>
2022-05-28 10:22:57 -0400

    add view for no pet and no consultation:

--- app/javascript/controllers/availability_controller.js
+++ app/javascript/controllers/availability_controller.js
@@ -1,20 +1,41 @@
 import { Controller } from "stimulus"
+import { csrfToken } from "@rails/ujs"
 
 export default class extends Controller {
   static targets = [ "available" ]
 
   connect() {
-    console.log('Hello!!');
+    console.log('Availability controller is now connected!!');
 
   }
 
   toggle(event) {
-    if (this.element.checked) {
-      console.log("toggle event checked");
-      console.log(this.element.outerHTML);
-    } else if (this.element.checked === false) {
-      console.log("toggle event unchecked");
-      console.log(this.element.innerHTML);
-    }
+    // if (this.element.checked) {
+    //   console.log("toggle event checked");
+    //   console.log(this.element.outerHTML);
+    // } else if (this.element.checked === false) {
+    //   console.log("toggle event unchecked");
+    //   console.log(this.element.innerHTML);
+    // }
+    let formData = new FormData();
+    formData.append("user[available]", this.availableTarget.checked )
+
+    fetch(this.data.get("update-url"), {
+      method: "PATCH",
+      dataType: 'script',
+      credentials: "include",
+      headers: { "X-CSRF-Token": getMetaValue("csrf-token") },
+    })
+      .then(function(response) {
+        if (response.status != 204) {
+            event.target.checked = !event.target.checked
+        }
+      })
+      .then((data) => {
+        console.log(data)
+      })
   }
+
+
+
 }
