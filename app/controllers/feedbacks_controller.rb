d7909aae7e73241f97c5833c50b179cf28f42ef7
Randja Raharijaona <randzoh@outlook.com>
2022-05-28 16:36:01 -0400

    refactored

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,5 +1,6 @@
 class FeedbacksController < ApplicationController
   before_action :find_user
+  before_action :find_consultation, only: [:new, :create]
 
   def show
     @feedback = Feedback.find(params[:id])
@@ -13,7 +14,7 @@ class FeedbacksController < ApplicationController
   def create
     @feedback = Feedback.new(feedback_params)
     @feedback.user = @user
-    @feedback.consultation = Consultation.find(params[:consultation_id])
+    @feedback.consultation = @consultation
 
     if @feedback.save!
       redirect_to receipt_path(@feedback.consultation)
@@ -30,5 +31,9 @@ class FeedbacksController < ApplicationController
 
   def feedback_params
     params.require(:feedback).permit(:user, :consultation, :rating, :vet_rating, :friend_rating, :comment)
+  end
+
+  def find_consultation
+    @consultation = Consultation.find(params[:consultation_id])
   end
 end
