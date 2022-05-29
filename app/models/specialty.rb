d6b29d48ebf3560d7154475a8dccca9128b4a644
Wendy Pham <wendypham_s2@hotmail.com>
2022-05-28 23:47:13 -0400

    add specialties to model

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,4 +1,17 @@
 class Specialty < ApplicationRecord
   has_many :user_specialties # Have to be specified in a end-to-end relation
   has_many :users, through: :user_specialties
+
+  SPECIALTIES = {
+    Avians: %w[ bird chicken owl parrot cockatoo budgy],
+    Equines: %w[ horse pony ],
+    Beef Cattles: %w[ cow ],
+    Felines: %w[ cat lynx tiger ],
+    Canines: %[ dog ],
+    Exotic Mammals: "",
+    Dairy Mammals: %[ cow  goat ],
+    Reptile: "",
+    Amphibian: %w[ frog ]
+  }
+
 end
