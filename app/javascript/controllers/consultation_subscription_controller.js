3b228adcc99913f2e50985bd1fc06a5b0a841084
nicolas fraisse <ni.fraisse@gmail.com>
2022-05-30 12:11:48 -0400

    Enhance structure

--- app/javascript/controllers/consultation_subscription_controller.js
+++ app/javascript/controllers/consultation_subscription_controller.js
@@ -6,40 +6,25 @@ export default class extends Controller {
   static targets = ['messages']
 
   connect() {
-    console.log('hey')
     this.channel = consumer.subscriptions.create(
-      { channel: 'NotificationsChannel' },
-      {
-        connected: function () {
-          // Called when the subscription is ready for use on the server
-          console.log('ok')
-        },
-
-        disconnected: function () {
-          // Called when the subscription has been terminated by the server
-        },
-
-        received: function (data) {
-          // Called when there's incoming data on the websocket for this channel
-          $('#notifications').prepend(data.html)
-        },
-      }
+      { channel: 'ConsultationChannel', id: this.consultationIdValue },
+      { received: (message) => this.#insertMessageAndScrollDown(message) }
     )
 
     console.log(`Subscribed to the consultation with the id ${this.consultationIdValue}.`)
   }
 
-  // #insertMessageAndScrollDown(message) {
-  //   this.messagesTarget.insertAdjacentHTML('beforeend', message)
-  //   this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-  // }
+  #insertMessageAndScrollDown(message) {
+    this.messagesTarget.insertAdjacentHTML('beforeend', message)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
 
-  // resetForm(event) {
-  //   event.target.reset()
-  // }
+  resetForm(event) {
+    event.target.reset()
+  }
 
-  // disconnect() {
-  //   console.log("Unsubscribed from the consultation's chat")
-  //   this.channel.unsubscribe()
-  // }
+  disconnect() {
+    console.log("Unsubscribed from the consultation's chat")
+    this.channel.unsubscribe()
+  }
 }
--- app/javascript/controllers/notification_subscription_controller.js
+++ app/javascript/controllers/notification_subscription_controller.js
@@ -9,30 +9,23 @@ export default class extends Controller {
       {
         connected: function () {
           // Called when the subscription is ready for use on the server
-          console.log('ok')
+          console.log('Notifications channel connected!')
         },
 
         disconnected: function () {
           // Called when the subscription has been terminated by the server
+          this.channel.unsubscribe()
         },
 
-        received: function (data) {
+        received: (data) => {
           // Called when there's incoming data on the websocket for this channel
-          document.getElementById('notifications').prepend(data.html)
+          this.#insertNotification(data.html)
         },
       }
     )
-
-    console.log(`Subscribed to the consultation with the id ${this.consultationIdValue}.`)
   }
 
-  #insertMessageAndScrollDown(message) {
-    this.messagesTarget.insertAdjacentHTML('beforeend', message)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-  }
-
-  disconnect() {
-    console.log("Unsubscribed from the consultation's chat")
-    this.channel.unsubscribe()
+  #insertNotification(message) {
+    this.notificationTarget.prepend(message)
   }
 }
