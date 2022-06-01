ce462115a0d4ca38008cbcc78f628d55d29b4fc7
Randja Raharijaona <randzoh@outlook.com>
2022-05-31 20:36:30 -0400

    receipt creating after feedback

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,16 +1,20 @@
 class ReceiptsController < ApplicationController
   def show
-    # @receipt = Receipt.find(params[:id])
+    @receipt = Receipt.where(consultation: params[:id])
+    @consultation = Consultation.find(params[:consultation_id])
   end
 
   def create
-    @receipt = Receipt.new(receipt_params)
+    @consultation = Consultation.find(params[:consultation_id])
+    @receipt = Receipt.new(consultation: @consultation)
+    @receipt.price_cents = 50
     @receipt.save!
+    redirect_to consultation_receipt_path(@consultation, @consultation.id)
   end
 
   private
 
   def receipt_params
-    params.require(:receipt).permit(:consultation, :price)
+    params.require(:receipt).permit(:consultation_id, :price_cents)
   end
 end
