2c8f267e089fbb8995f4475c6b29f83f5d97e5cb
Wendy Pham <wendypham_s2@hotmail.com>
2022-06-02 18:36:54 -0400

    add more vets

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,40 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+require 'open-uri'
+require 'json'
+
+
+TEACHERS = {
+  teacher1: {
+    name: "Kristyna Dierstein",
+    address: Faker::Address.full_address,
+    username: "kristynadierstein",
+  },
+  teacher2: {
+    name: "Jeff Trempe",
+    address: Faker::Address.full_address,
+    username: "JefGreen",
+  },
+  teacher3: {
+    name: "Frederic Charest",
+    address: Faker::Address.full_address,
+    username: "aquidam",
+  },
+  teacher4: {
+    name: "Solene Duprat",
+    address: Faker::Address.full_address,
+    username: "aquidam",
+  }
+}
+
+def find_pic(username)
+  url = "https://api.github.com/users/#{username}"
+  user_serialized = URI.open(url).read
+  user = JSON.parse(user_serialized)
+  user["avatar_url"]
+end
+
 
 p "Destroying old seeds..."
 
@@ -28,12 +62,13 @@ p "Starting new seeds..."
 unless Specialty.any?
   specialties = ["Avians",
     "Equines",
-    "Beef Cattles",
+    "Farm Animals",
     "Felines",
     "Canines",
     "Exotic Mammals",
     "Dairy Mammals",
-    "Reptile and Amphibian"]
+    "Reptile and Amphibian",
+    "Generalist"]
 
   specialties.each do |s|
     specialty = Specialty.new
@@ -43,7 +78,9 @@ unless Specialty.any?
   end
 end
 
+# ========================
 # Creating vet
+# ========================
 vet_1 = User.create!({
   email: "drfraisse@mypethealth.ca",
   password: "123456",
@@ -51,9 +88,60 @@ vet_1 = User.create!({
   address: "391 Rue de la Congrégation, Montreal QC",
   available: true})
 
+# Add vet photo
+file = URI.open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1570482026/xlayp6fkanfrqzwlg1eu.jpg')
+vet_1.photo.attach(io: file, filename: 'nic-avatar.jpg', content_type: 'image/jpg')
 # Adding 2 specialties to vet
-vet_1.specialties = Specialty.all.sample(2)
+vet_1.specialties = [Specialty.find_by(name:"Canines"), Specialty.find_by(name:"Felines"), Specialty.find_by(name:"Generalist") ]
 
+
+vet_2 = User.create!({
+  email: "drlafontaine@mypethealth.ca",
+  password: "123456",
+  name: "Stephane Lafontaine",
+  address: "1155 rue Metcalfe, Montreal QC",
+  available: true})
+
+# Add vet photo
+file = URI.open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1652365690/ygqzrfi0zo1jpj6y4lwa.jpg')
+vet_2.photo.attach(io: file, filename: 'stephane-avatar.jpg', content_type: 'image/jpg')
+# Adding 2 specialties to vet
+vet_2.specialties = [Specialty.find_by(name:"Equines"), Specialty.find_by(name:"Farm Animals")]
+
+vet_3 = User.create!({
+  email: "drleger@mypethealth.ca",
+  password: "123456",
+  name: "Enya Leger",
+  address: "2209 ave du Mont-Royal Est, Montreal QC",
+  available: true})
+
+# Add vet photo
+file = URI.open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1641386359/dhel4rtasvauuahid89k.jpg')
+vet_3.photo.attach(io: file, filename: 'enya-avatar.jpg', content_type: 'image/jpg')
+# Adding 2 specialties to vet
+vet_3.specialties = [Specialty.find_by(name:"Felines"), Specialty.find_by(name:"Exotic Mammals")]
+
+
+# TEACHERS.each do |k, v|
+#   pic = find_pic(k[:username])
+#   vet = User.create!({
+#     email: "#{k[:name]}@mypethealth.ca",
+#     password: "123456",
+#     name: k[:name],
+#     address: k[:address],
+#     available: true
+#   })
+#    # Add vet photo
+#   vet.photo.attach(io: pic, filename: 'user-avatar.jpg', content_type: 'image/jpg')
+#   p "#{vet.name} has been created"
+
+# end
+
+
+
+# ======================
+# Creating clients
+# ======================
 # Creating client 1
 client_1 = User.create!({ email: "jeff@ilovecats.com", password: "123456", name: "Jeff Trempe", address: "1940 Jolicoeur Street, Montreal QC" })
 
@@ -99,7 +187,7 @@ end
 end
 
 # Creating random vets to populate vets list
-5.times do
+15.times do
   user = User.create!({
   email: Faker::Internet.email,
   password: "password",
@@ -108,6 +196,12 @@ end
   available: true})
 
   user.specialties = Specialty.all.sample(2)
+
+  # Add vet photo
+  file = URI.open('https://source.unsplash.com/random/?face-avatar')
+  user.photo.attach(io: file, filename: 'user-avatar.jpg', content_type: 'image/jpg')
+  # Adding 2 specialties to vet
+
 end
 
 p "Seeding is complete."
