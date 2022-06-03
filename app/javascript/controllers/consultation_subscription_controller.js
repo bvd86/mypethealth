4ef23ffe8804eff67fbf4a2c3b7aaae6033a676c
Billy Veillette-Daigle <bvd86@icloud.com>
2022-06-03 09:44:16 -0400

    Added the user ID sender/receiver to the chat. Still doesnt refresh on its own. If you can find the issue go ahead.

--- app/javascript/controllers/consultation_subscription_controller.js
+++ app/javascript/controllers/consultation_subscription_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import consumer from "../channels/consumer"
 
 export default class extends Controller {
-  static values = { consultationId: Number }
+  static values = { consultationId: Number, currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
@@ -14,11 +14,32 @@ export default class extends Controller {
     console.log(`Subscribed to the consultation with the id ${this.consultationIdValue}.`)
   }
 
-  #insertMessageAndScrollDown(message) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", message)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  #insertMessageAndScrollDown(data) {
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+     this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+  this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
 
   resetForm(event) {
     event.target.reset()
