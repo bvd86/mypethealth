32038bb9b9e708aafb3300b9bb5d87b9b92a1dc6
Wendy Pham <wendypham_s2@hotmail.com>
2022-06-08 12:24:41 -0400

    testing diff dom manipulation

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -6,11 +6,22 @@ export default class extends Controller {
   connect() {
     console.log('Dropdown controller is connected!');
     document.addEventListener("click", () => {
-      this.contentTarget.classList.toggle("show");
+      console.log(this.event.target)
+      if (this.event.target != this.contentTarget) {
+      this.contentTarget.classList.remove("show");
+      }
+
     })
   }
 
   showContent() {
-    this.contentTarget.classList.toggle("show");
+
+      this.contentTarget.classList.toggle("show");
+      console.log(this.event.target)
+    }
+
+  hideContent() {
+
+    this.contenTarget.classList.remove("show");
   }
 }
